a217419f5c071f774116929fb34b4027ef6b672b
Harry Shao <ysharry2020@gmail.com>
2015-09-26 08:42:39 -0400

    computing coordination

--- HelloWorld/app/src/main/java/org/hackcmu/helloworld/MapView.java
+++ HelloWorld/app/src/main/java/org/hackcmu/helloworld/MapView.java
@@ -19,17 +19,36 @@ public class MapView extends View {
     private Bitmap locator;
     private int[] x = {156,91,25,95,55};
     private int[] y = {14,200,288,258,322};
-    private int[] dis = {0,10000,5000,4500,4500};
+    private int[] dis = {0,10000,15000,19500,24000};
+    private float xoffset = 2.5f;
+    private float yoffset = 2.5f;
 
     public MapView(Context context, AttributeSet attrs){
         super(context,attrs);
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.map);
-        locator = BitmapFactory.decodeByteArray(getResources(),R.drawable.);
+        locator = BitmapFactory.decodeResource(getResources(), R.drawable.ico_paw);
     }
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
         canvas.drawBitmap(bmp, 0, 0, null);
 
+        /*int i = 0;
+        while(i < 5 && steps >= dis[i]){
+            i++;
+        }
+        i--;
+        int exceed_amount = steps - dis[i];
+        int xdiff, ydiff,a,b,interv;
+        if(i < 4){
+            xdiff = x[i+1]-x[i];
+            ydiff = y[i+1]-y[i];
+            interv = dis[i+1]-dis[i];
+            a = x[i] + xdiff * exceed_amount/interv;
+            b = y[i] + ydiff * exceed_amount/interv;
+        }*/
+
+
+        canvas.drawBitmap(locator,x[0]*xoffset,y[0]*yoffset,null);
 
 
 
