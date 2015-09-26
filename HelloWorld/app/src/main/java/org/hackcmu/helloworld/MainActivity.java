226aa2c9e06f7c4ba341596f47235ec4c697726f
Billy Zhu <zyx.billy@gmail.com>
2015-09-25 23:39:55 -0400

    add Google Play Services Fitness code

--- HelloWorld/app/src/main/java/org/hackcmu/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/org/hackcmu/helloworld/MainActivity.java
@@ -1,24 +1,229 @@
 package org.hackcmu.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResult;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_OAUTH = 1;
+
+    /**
+     *  Track whether an authorization activity is stacking over the current activity, i.e. when
+     *  a known auth error is being resolved, such as showing the account chooser or presenting a
+     *  consent dialog. This avoids common duplications as might happen on screen rotations, etc.
+     */
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+
+    private GoogleApiClient mClient = null;
+
+    private static final String DATE_FORMAT = "dd/M/yyyy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleApiClient client = new GoogleApiClient.Builder(this)
+        buildFitnessClient();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect to the Fitness API
+        Log.i(LOG_TAG, "Connecting...");
+        mClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mClient.isConnected()) {
+            mClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_OAUTH) {
+            authInProgress = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mClient.isConnecting() && !mClient.isConnected()) {
+                    mClient.connect();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(AUTH_PENDING, authInProgress);
+    }
+
+    /**
+     *  Build a {@link GoogleApiClient} that will authenticate the user and allow the application
+     *  to connect to Fitness APIs. The scopes included should match the scopes your app needs
+     *  (see documentation for details). Authentication will occasionally fail intentionally,
+     *  and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
+     *  can address. Examples of this include the user never having signed in before, or having
+     *  multiple accounts on the device and needing to specify which account to use, etc.
+     */
+    private void buildFitnessClient() {
+        // Create the Google API Client
+        mClient = new GoogleApiClient.Builder(this)
                 .addApi(Fitness.HISTORY_API)
-        .build();
-        client.connect();
+                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
+                .addConnectionCallbacks(
+                        new GoogleApiClient.ConnectionCallbacks() {
+
+                            @Override
+                            public void onConnected(Bundle bundle) {
+                                Log.i(LOG_TAG, "Connected!!!");
+                                // Now you can make calls to the Fitness APIs.
+                                // Put application specific code here.
+
+                                // Setting a start and end date using a range of 1 week before this moment.
+                                Calendar cal = Calendar.getInstance();
+                                Date now = new Date();
+                                cal.setTime(now);
+                                long endTime = cal.getTimeInMillis();
+                                cal.add(Calendar.WEEK_OF_YEAR, -1);
+                                long startTime = cal.getTimeInMillis();
+
+                                SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+                                Log.i(LOG_TAG, "Range Start: " + dateFormat.format(startTime));
+                                Log.i(LOG_TAG, "Range End: " + dateFormat.format(endTime));
+
+                                DataReadRequest readRequest = new DataReadRequest.Builder()
+                                        // The data request can specify multiple data types to return, effectively
+                                        // combining multiple data queries into one call.
+                                        // In this example, it's very unlikely that the request is for several hundred
+                                        // datapoints each consisting of a few steps and a timestamp.  The more likely
+                                        // scenario is wanting to see how many steps were walked per day, for 7 days.
+                                        .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                                                // Analogous to a "Group By" in SQL, defines how data should be aggregated.
+                                                // bucketByTime allows for a time span, whereas bucketBySession would allow
+                                                // bucketing by "sessions", which would need to be defined in code.
+                                        .bucketByTime(1, TimeUnit.DAYS)
+                                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                                        .build();
+
+                                // Invoke the History API to fetch the data with the query and await the result of
+                                // the read request.
+                                PendingResult<DataReadResult> pendingResult =
+                                        Fitness.HistoryApi.readData(mClient, readRequest);
+
+                                pendingResult.setResultCallback(
+                                        new ResultCallback<DataReadResult>() {
+                                            @Override
+                                            public void onResult(DataReadResult dataReadResult) {
+                                                Log.d(LOG_TAG, "got result!!!, size: " + dataReadResult.getBuckets().size());
+                                                if (dataReadResult.getBuckets().size() > 0) {
+                                                    for (Bucket bucket : dataReadResult.getBuckets()) {
+                                                        List<DataSet> dataSets = bucket.getDataSets();
+                                                        for (DataSet dataSet : dataSets) {
+                                                            // Show the data points
+                                                            processDataSet(dataSet);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                );
+
+                                Log.d(LOG_TAG, "callback set");
+
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                // If your connection to the sensor gets lost at some point,
+                                // you'll be able to determine the reason and react to it here.
+                                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                    Log.i(LOG_TAG, "Connection lost.  Cause: Network Lost.");
+                                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                    Log.i(LOG_TAG, "Connection lost.  Reason: Service Disconnected");
+                                }
+                            }
+                        }
+                )
+                .addOnConnectionFailedListener(
+                        new GoogleApiClient.OnConnectionFailedListener() {
+                            // Called whenever the API client fails to connect.
+                            @Override
+                            public void onConnectionFailed(ConnectionResult result) {
+                                Log.i(LOG_TAG, "Connection failed. Cause: " + result.toString());
+                                if (!result.hasResolution()) {
+                                    // Show the localized error dialog
+                                    GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+                                            MainActivity.this, 0).show();
+                                    return;
+                                }
+                                // The failure has a resolution. Resolve it.
+                                // Called typically when the app is not yet authorized, and an
+                                // authorization dialog is displayed to the user.
+                                if (!authInProgress) {
+                                    try {
+                                        Log.i(LOG_TAG, "Attempting to resolve failed connection");
+                                        authInProgress = true;
+                                        result.startResolutionForResult(MainActivity.this,
+                                                REQUEST_OAUTH);
+                                    } catch (IntentSender.SendIntentException e) {
+                                        Log.e(LOG_TAG,
+                                                "Exception while starting resolution activity", e);
+                                    }
+                                }
+                            }
+                        }
+                )
+                .build();
+    }
+
+    private void processDataSet(DataSet dataSet) {
+        Log.i(LOG_TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.i(LOG_TAG, "Data point:");
+            Log.i(LOG_TAG, "\tType: " + dp.getDataType().getName());
+            Log.i(LOG_TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i(LOG_TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for(Field field : dp.getDataType().getFields()) {
+                Log.i(LOG_TAG, "\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
     }
 
     @Override
